0da603f047914edf570bc1fa1a0f04191f0f79d8
AzarioCossa <cossaazario@gmail.com>
2024-10-10 09:32:56 +0200

    BugFix : declaration of passwordChars array fix

--- sha256conversion.py
+++ sha256conversion.py
@@ -2,13 +2,13 @@ import hashlib
 import random
 import string
 
-def generate_password(str1, str2):
-    passwordChars={}
+def generate_password(str1, str2, size):
+    passwordChars=[]
     combined_str = str1 + str2
     
     hashed = hashlib.sha256(combined_str.encode()).hexdigest()
     
-    password = hashed[:8]
+    password = hashed[:size]
     
     if (not any(c.isupper() for c in password) or
         not any(c.islower() for c in password) or
@@ -17,7 +17,7 @@ def generate_password(str1, str2):
 
         all_chars = string.ascii_letters + string.digits + string.punctuation
 
-        for _ in range(8):
+        for _ in range(size):
             random_char = random.choice(all_chars)
             passwordChars.append(random_char)
         password = ''.join(passwordChars)
@@ -25,7 +25,9 @@ def generate_password(str1, str2):
     return password
 
 if __name__ == "__main__":
-    str1 = "Texto1"
-    str2 = "Texto2"
-    password = generate_password(str1, str2)
+    
+    size=12
+    text1 = "Texto1"
+    text2 = "Texto2"
+    password = generate_password(text1, text2, size)
     print(password)
